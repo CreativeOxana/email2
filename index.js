4e0be7f2de9b4233a4f83b013b14411aee602907
Oxana Chizitcaia <creativesouloxana@gmail.com>
2024-05-05 21:27:26 +0200

    Adding fillSubject, fillBody, goodbye js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const goodbye = (name) => {
+  return `Na shledanou, ${name}`;
+};
 
+const fillSubject = (subject) => {
+  const subjectElm = document.querySelector('.email__subject');
+  subjectElm.textContent = subject;
+};
+
+const fillBody = (body, name) => {
+  const bodyElm = document.querySelector('.email__body');
+  bodyElm.textContent = body;
+  const byeELm = document.querySelector('.email__closing');
+  byeELm.textContent = goodbye(name);
+};
+
+fillSubject('Nepřišly gumičky');
+fillBody(
+  'Nepřišly gumičky. A co na to počítač? Mlčí. No tak to je konec. Všecko vylejt.',
+  'referent Kubrt',
+);
+// goodbye('Pavel Ovesný');
